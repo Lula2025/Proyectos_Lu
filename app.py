81c12768ee05563e71b64da1af1bd2e0ef2f19f4
Lula2025 <rortega_mdl@hotmail.com>
2025-05-14 16:54:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.markdown("""
     <style>
         /* Modificar los márgenes de la página */
         .main {
-            margin: 50px 150px;  /* 50px arriba y abajo, 150px a la izquierda y derecha */
+            margin: 100px 100px;  /* 50px arriba y abajo, 150px a la izquierda y derecha */
         }
 
         /* Espacio entre las gráficas y otros elementos */
